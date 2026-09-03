9610db1c02cc38db79854897df58f1ee5931f6b4
Ximenalopez07 <helenhenao8@gmail.com>
2026-09-02 19:59:32 -0500

    Configurar Vite para Railway

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
       '/api': {
@@ -12,5 +12,9 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+  },
+
+  preview: {
+    allowedHosts: ['distinguished-vibrancy-production.up.railway.app'],
   },
 })
